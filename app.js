9dfa9a9b12055b02a5d5f12c2c2344700b41a48d
Laurens <laurens.de.smedt.pc@hotmail.com>
2023-03-02 19:45:34 +0100

    labo 1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/foto', (req, res) => {
+    res.sendFile(__dirname + '/public/download.jpg');
+  });
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
